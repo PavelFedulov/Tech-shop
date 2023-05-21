8ea75b9d4248758c56736b80ddb9665230f98f37
Pavel <125391319+PavelFedulov@users.noreply.github.com>
2023-05-21 14:08:10 +0400

    added cards on js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,53 @@
 //общие тэги
-const body = document.querySelector('body');
-const wrapper = document.createElement('div');
+const body = document.querySelector("body");
+const wrapper = document.createElement("div");
+const wrapperFull = document.createElement("div");
 
 //Header start
 
 //переменные тэгов
-const header = document.createElement('header');
-const headerWrapper = document.createElement('div');
-const headerLogo = document.createElement('div');
-const headerLogoLink = document.createElement('a');
-const headerTitle = document.createElement('h1');
-const headerNav = document.createElement('div');
-const headerList = document.createElement('ul');
+
+//header
+const header = document.createElement("header");
+const headerWrapper = document.createElement("div");
+const headerLogo = document.createElement("div");
+const headerLogoLink = document.createElement("a");
+const headerTitle = document.createElement("h1");
+const headerNav = document.createElement("div");
+const headerList = document.createElement("ul");
+
+//section: apple Tags
+const appleSection = document.createElement("section");
+const appleCaption = document.createElement("div");
+const appleTitle = document.createElement("h2");
+const appleCards = document.createElement("div");
 
 //классы тэгов
-header.classList.add('header')
-wrapper.classList.add('wrapper');
-headerWrapper.classList.add('header__wrapper');
-headerLogo.classList.add('header__logo');
-headerLogoLink.classList.add('header__logo-link');
-headerTitle.classList.add('header__title');
-headerNav.classList.add('header__nav');
-headerList.classList.add('header__list');
+
+//header
+header.classList.add("header");
+wrapper.classList.add("wrapper");
+headerWrapper.classList.add("header__wrapper");
+headerLogo.classList.add("header__logo");
+headerLogoLink.classList.add("header__logo-link");
+headerTitle.classList.add("header__title");
+headerNav.classList.add("header__nav");
+headerList.classList.add("header__list");
+
+//section: apple
+appleSection.classList.add("apple");
+wrapperFull.classList.add("wrapper-full");
+appleCaption.classList.add("apple__caption", "section__caption");
+appleTitle.classList.add("apple__title", "section__title");
+appleCards.classList.add("apple__cards", "section__cards");
 
 //заполнение тэгов
-headerTitle.textContent = 'Tech shop';
+headerTitle.textContent = "Tech shop";
+appleTitle.textContent = "Let's see what we have from Apple";
 
 //сборка тэгов
+
+//header
 body.appendChild(header);
 header.appendChild(wrapper);
 wrapper.appendChild(headerWrapper);
@@ -36,59 +57,79 @@ headerLogoLink.appendChild(headerTitle);
 headerWrapper.appendChild(headerNav);
 headerNav.appendChild(headerList);
 
+//section: apple
+body.appendChild(appleSection);
+appleSection.appendChild(wrapperFull);
+wrapperFull.appendChild(appleCaption);
+appleCaption.appendChild(appleTitle);
+wrapperFull.appendChild(appleCards);
+
 //массив объектов headerList
 const headerItemsArray = [
-  {navPoint: 'Apple'},
-  {navPoint: 'Samsung'},
-  {navPoint: 'Xiaomi'},
+  { navPoint: "Apple" },
+  { navPoint: "Samsung" },
+  { navPoint: "Xiaomi" },
 ];
 
 //функция создания элементов меню навигации
 const createHeaderHTMLList = (navPoint) => {
-return `<li class = "header__item">
+  return `<li class = "header__item">
     <a href = "#${navPoint}" class = "header__link">
     ${navPoint}
     </a>
-    </li>`
-}
+    </li>`;
+};
 
 //создаем переменную с HTML кодом из функции
-const headerListHTML = headerItemsArray.map(navPointName => {
-  return createHeaderHTMLList(navPointName.navPoint);
-}).join('')
+const headerListHTML = headerItemsArray
+  .map((navPointName) => {
+    return createHeaderHTMLList(navPointName.navPoint);
+  })
+  .join("");
 //вставляем HTML в headerList
 headerList.innerHTML = headerListHTML;
-//вствыляем headerList в headerNav
-headerNav.insertAdjacentElement(headerList);
 
 //Header end
 
+//Apple block start
+
+//функция создания карточки
+const generateAppleCard = (pic, model, year) => {
+  return `
+  <div class="apple__card section__card">
+      <img class = "apple__card-pic section__card-pic" src=${pic} alt="macbook pro" onClick="imgsrc();">
+      <h3 class="apple__card-title section__card-title">
+        ${model}
+      </h3>
+      <p class="apple__card-subtitle section__card-subtitle">
+        ${year}
+      </p>
+      <button class="bt__buy">Купить</button>
+  </div>
+  `;
+};
 
 
+//массив с картинками карточек apple
+const appleMBPPicsArray = [
+  "./img/MacBook/MacBook_Pro_1.jpg",
+  "./img/MacBook/MacBook_Pro_2.jpg",
+  "./img/MacBook/MacBook_Pro_3.jpg",
+  "./img/MacBook/MacBook_Pro_4.jpg",
+];
 
+const appleCardList = [
+  { pic: appleMBPPicsArray[0], model: "MacBook Pro", year: 2015 },
+  { pic: appleMBPPicsArray[1], model: "MacBook Pro", year: 2015 },
+];
 
+const appleCardHTML = appleCardList
+  .map((card) => {
+    return generateAppleCard(card.pic, card.model, card.year);
+  })
+  .join("");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+appleCards.innerHTML = appleCardHTML;
 
 // плавная прокрутка
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
