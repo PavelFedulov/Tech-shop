4693b4e6fb40d88919be4db804bc7d7d09500e48
Pavel <125391319+PavelFedulov@users.noreply.github.com>
2023-05-22 23:38:50 +0400

    completed cards, footer, order_card (without animation)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 //общие тэги
 const body = document.querySelector("body");
 const wrapper = document.createElement("div");
-const wrapperFull = document.createElement("div");
-const buttonUp = document.createElement('div');
-//Header start
+const wrapperFullApple = document.createElement("div");
+const wrapperFullSamsung = document.createElement("div");
+const wrapperFullXiaomi = document.createElement("div");
+const wrapperFooter = document.createElement("div");
+const buttonUp = document.createElement("div");
+const buttonUpPic = document.createElement("img");
+//----------------------------------------------------------------------------
 
 //переменные тэгов
 
@@ -16,13 +20,60 @@ const headerTitle = document.createElement("h1");
 const headerNav = document.createElement("div");
 const headerList = document.createElement("ul");
 
-//section: apple Tags
+//section: apple
 const appleSection = document.createElement("section");
 const appleCaption = document.createElement("div");
 const appleTitle = document.createElement("h2");
 const appleCards = document.createElement("div");
 
+//section: samsung
+const samsungSection = document.createElement("section");
+const samsungCaption = document.createElement("div");
+const samsungTitle = document.createElement("h2");
+const samsungCards = document.createElement("div");
+
+//section: xiaomi
+const xiaomiSection = document.createElement("section");
+const xiaomiCaption = document.createElement("div");
+const xiaomiTitle = document.createElement("h2");
+const xiaomiCards = document.createElement("div");
+
+//footer
+const footer = document.createElement("footer");
+const footerLogo = document.createElement("div");
+const footerTitle = document.createElement("h2");
+const footerNav = document.createElement("footerNav");
+const footerList = document.createElement("ul");
+
+//order form
+const formBody = document.createElement("div");
+const form = document.createElement("div");
+const orderForm = document.createElement("form");
+const orderWrapper = document.createElement("div");
+const quantityBlock = document.createElement("div");
+const quantityLabel = document.createElement("div");
+const quantityTitle = document.createElement("label");
+const orderQuantity = document.createElement("input");
+const colorBlock = document.createElement("div");
+const colorGroupTitle = document.createElement("p");
+const colorGroup = document.createElement("ul");
+const commentBlock = document.createElement("div");
+const commentTitle = document.createElement("div");
+const commentLabel = document.createElement("label");
+const commentField = document.createElement("textarea");
+const formButtons = document.createElement("div");
+const formBuyButton = document.createElement("button");
+const formCloseButton = document.createElement("button");
+
+//----------------------------------------------------------------------------
+
 //классы тэгов
+
+//общие
+
+buttonUp.classList.add("btn__up", "btn__up-hide");
+buttonUpPic.classList.add("btn__up-pic");
+buttonUpPic.setAttribute("src", "../img/btn-up.png");
 
 //header
 header.classList.add("header");
@@ -33,22 +84,95 @@ headerLogoLink.classList.add("header__logo-link");
 headerTitle.classList.add("header__title");
 headerNav.classList.add("header__nav");
 headerList.classList.add("header__list");
-buttonUp.classList.add('btn__up', 'btn__up-hide');
 
 //section: apple
-appleSection.setAttribute('id', 'Apple')
+appleSection.setAttribute("id", "Apple");
 appleSection.classList.add("apple");
-wrapperFull.classList.add("wrapper-full");
+wrapperFullApple.classList.add("wrapper-full");
 appleCaption.classList.add("apple__caption", "section__caption");
 appleTitle.classList.add("apple__title", "section__title");
 appleCards.classList.add("apple__cards", "section__cards");
 
+//section: samsung
+wrapperFullSamsung.classList.add("wrapper-full");
+samsungSection.setAttribute("id", "Samsung");
+samsungSection.classList.add("samsung");
+samsungCaption.classList.add("samsung__caption", "section__caption");
+samsungTitle.classList.add("samsung__title", "section__title");
+samsungCards.classList.add("samsung__cards", "section__cards");
+
+//section: xiaomi
+wrapperFullXiaomi.classList.add("wrapper-full");
+xiaomiSection.setAttribute("id", "Xiaomi");
+xiaomiSection.classList.add("xiaomi");
+xiaomiCaption.classList.add("xiaomi__caption", "section__caption");
+xiaomiTitle.classList.add("xiaomi__title", "section__title");
+xiaomiCards.classList.add("xiaomi__cards", "section__cards");
+
+//footer
+
+footer.classList.add("footer");
+footerNav.classList.add("footer__nav");
+wrapperFooter.classList.add("wrapper");
+footerList.classList.add("footer__list");
+footerLogo.classList.add("footer__logo");
+footerTitle.classList.add("footer__title");
+
+//order__form
+
+formBody.classList.add("form__body", "form__body-hide");
+form.classList.add("form");
+orderForm.classList.add("order__form");
+orderWrapper.classList.add("order__wrapper");
+quantityBlock.classList.add("quantity__block");
+quantityLabel.classList.add("quantity__label");
+quantityTitle.classList.add("quantity__title");
+quantityTitle.setAttribute("for", "order__quantity");
+orderQuantity.setAttribute("type", "number");
+orderQuantity.setAttribute("min", "1");
+orderQuantity.setAttribute("placeholder", "1");
+orderQuantity.classList.add("order__quantity");
+colorBlock.classList.add("color__block");
+colorGroupTitle.classList.add("color__group-title");
+colorGroup.classList.add("color__group");
+commentBlock.classList.add("comment");
+commentTitle.classList.add("comment__title");
+commentLabel.classList.add("comment__title");
+commentField.classList.add("comment__field");
+commentField.setAttribute("name", "comment");
+commentField.setAttribute("id", "comment");
+commentField.setAttribute("cols", "20");
+commentField.setAttribute("rows", "10");
+commentField.setAttribute("maxlength", "2000");
+formButtons.classList.add("form__buttons");
+formBuyButton.classList.add("bt__buy", "form__bt-buy");
+formBuyButton.setAttribute("type", "submit");
+formBuyButton.setAttribute("id", "buy-button");
+formCloseButton.classList.add("bt__buy", "form__bt-close");
+formCloseButton.setAttribute("type", "reset");
+formCloseButton.setAttribute("id", "close-button");
+
+//----------------------------------------------------------------------------
 
 //заполнение тэгов
 headerTitle.textContent = "Tech shop";
 appleTitle.textContent = "Let's see what we have from Apple";
+samsungTitle.textContent = "That's what we have by Samsung";
+xiaomiTitle.textContent = "And what about Xiaomi";
+footerTitle.textContent = "Tech Shop";
+quantityTitle.textContent = "Specify the number of goods";
+colorGroupTitle.textContent = "Select device color:";
+commentTitle.textContent = "Enter a comment to the order:";
+formBuyButton.textContent = "BUY";
+formCloseButton.textContent = "CLOSE";
+
+//----------------------------------------------------------------------------
 
 //сборка тэгов
+
+//общие
+
+buttonUp.appendChild(buttonUpPic);
 
 //header
 body.appendChild(buttonUp);
@@ -60,13 +184,60 @@ headerLogo.appendChild(headerLogoLink);
 headerLogoLink.appendChild(headerTitle);
 headerWrapper.appendChild(headerNav);
 headerNav.appendChild(headerList);
+buttonUp.appendChild(buttonUpPic);
 
 //section: apple
 body.appendChild(appleSection);
-appleSection.appendChild(wrapperFull);
-wrapperFull.appendChild(appleCaption);
+appleSection.appendChild(wrapperFullApple);
+wrapperFullApple.appendChild(appleCaption);
 appleCaption.appendChild(appleTitle);
-wrapperFull.appendChild(appleCards);
+wrapperFullApple.appendChild(appleCards);
+
+//section: samsung
+body.appendChild(samsungSection);
+samsungSection.appendChild(wrapperFullSamsung);
+wrapperFullSamsung.appendChild(samsungCaption);
+samsungCaption.appendChild(samsungTitle);
+wrapperFullSamsung.appendChild(samsungCards);
+
+//section: xiaomi
+body.appendChild(xiaomiSection);
+xiaomiSection.appendChild(wrapperFullXiaomi);
+wrapperFullXiaomi.appendChild(xiaomiCaption);
+xiaomiCaption.appendChild(xiaomiTitle);
+wrapperFullXiaomi.appendChild(xiaomiCards);
+
+//footer
+body.appendChild(footer);
+footer.appendChild(wrapperFooter);
+wrapperFooter.appendChild(footerLogo);
+footerLogo.appendChild(footerTitle);
+wrapperFooter.appendChild(footerNav);
+footerNav.appendChild(footerList);
+
+//order__form
+body.appendChild(formBody);
+formBody.append(form);
+form.appendChild(orderForm);
+orderForm.appendChild(orderWrapper);
+orderWrapper.appendChild(quantityBlock);
+quantityBlock.appendChild(quantityLabel);
+quantityLabel.appendChild(quantityTitle);
+quantityBlock.appendChild(orderQuantity);
+orderWrapper.appendChild(colorBlock);
+colorBlock.appendChild(colorGroupTitle);
+colorBlock.appendChild(colorGroup);
+orderWrapper.appendChild(commentBlock);
+commentBlock.appendChild(commentLabel);
+commentLabel.appendChild(commentTitle);
+commentBlock.appendChild(commentField);
+orderWrapper.appendChild(formButtons);
+formButtons.appendChild(formBuyButton);
+formButtons.appendChild(formCloseButton);
+
+//----------------------------------------------------------------------------
+
+//Header
 
 //массив объектов headerList
 const headerItemsArray = [
@@ -94,11 +265,11 @@ const headerListHTML = headerItemsArray
 headerList.innerHTML = headerListHTML;
 
 //Header end
-
+//----------------------------------------------------------------------------
 //Apple block start
 
 //функция создания карточки
-const generateAppleCard = (pic, model, year) => {
+const generateAppleCard = (pic, model, date) => {
   return `
   <div class="apple__card section__card">
       <img class = "apple__card-pic section__card-pic" src=${pic} alt="macbook pro" onClick="imgsrc();">
@@ -106,7 +277,7 @@ const generateAppleCard = (pic, model, year) => {
         ${model}
       </h3>
       <p class="apple__card-subtitle section__card-subtitle">
-        ${year}
+        ${date}
       </p>
       <div class = 'bt__buy-block'>
       <button class="bt__buy">Купить</button>
@@ -115,21 +286,38 @@ const generateAppleCard = (pic, model, year) => {
   `;
 };
 
-
-//массив с картинками карточек apple
-const appleMBPPicsArray = [
-  './img/AppleImg/MacBook_Pro_2.jpg',
-  './img/AppleImg/iPhone_12_1.jpg',
-  './img/AppleImg/Apple_Watch_1.jpg'
-];
-
+//массив объектов карточек apple
 const appleCardList = [
-  { pic: appleMBPPicsArray[0], model: "MacBook Pro", year: 2015 },
-  { pic: appleMBPPicsArray[1], model: "iPhone 12", year: 2014 },
-  { pic: appleMBPPicsArray[2], model: "Apple Watch Series 8", year: 2022 },
-  { pic: appleMBPPicsArray[1], model: "iPhone 12", year: 2014 },
-  { pic: appleMBPPicsArray[1], model: "iPhone 12", year: 2014 },
-
+  {
+    pic: "./img/AppleImg/MacBook_Pro_2.jpg",
+    model: "MacBook Pro",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/iPhone_12_1.jpg",
+    model: "iPhone 12",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/Apple_Watch_1.jpg",
+    model: "Apple Watch Series 8",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/MacBook_Pro_1.jpg",
+    model: "MacBook Pro",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/MacBook_Pro_3.jpg",
+    model: "MacBook Pro",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/MacBook_Pro_4.jpg",
+    model: "MacBook Pro",
+    date: getDateForCard(),
+  },
 ];
 
 const appleCardHTML = appleCardList
@@ -139,7 +327,211 @@ const appleCardHTML = appleCardList
   .join("");
 
 appleCards.innerHTML = appleCardHTML;
+//----------------------------------------------------------------------------
+//samsung block start
 
+//массив объектов карточек samsung
+const samsungCardList = [
+  {
+    pic: "./img/AppleImg/MacBook_Pro_2.jpg",
+    model: "Samsung",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/iPhone_12_1.jpg",
+    model: "Samsung",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/Apple_Watch_1.jpg",
+    model: "Samsung",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/MacBook_Pro_1.jpg",
+    model: "Samsung",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/MacBook_Pro_3.jpg",
+    model: "Samsung",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/MacBook_Pro_4.jpg",
+    model: "Samsung",
+    date: getDateForCard(),
+  },
+];
+
+//функция создания карточки
+const generateSamsungCard = (pic, model, date) => {
+  return `
+  <div class="samsung__card section__card">
+      <img class = "samsung__card-pic section__card-pic" src=${pic} alt="macbook pro" onClick="imgsrc();">
+      <h3 class="samsung__card-title section__card-title">
+        ${model}
+      </h3>
+      <p class="apple_samsung-subtitle section__card-subtitle">
+        ${date}
+      </p>
+      <div class = 'bt__buy-block'>
+      <button class="bt__buy">Купить</button>
+      </div>
+  </div>
+  `;
+};
+
+const samsungCardHTML = samsungCardList
+  .map((card) => {
+    return generateSamsungCard(card.pic, card.model, card.year);
+  })
+  .join("");
+
+samsungCards.innerHTML = samsungCardHTML;
+//----------------------------------------------------------------------------
+//xiaomi block start
+
+//массив объектов карточек xiaomi
+const xiaomiCardList = [
+  {
+    pic: "./img/AppleImg/MacBook_Pro_2.jpg",
+    model: "Xiaomi",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/iPhone_12_1.jpg",
+    model: "Xiaomi",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/Apple_Watch_1.jpg",
+    model: "Xiaomi",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/MacBook_Pro_1.jpg",
+    model: "Xiaomi",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/MacBook_Pro_3.jpg",
+    model: "Xiaomi",
+    date: getDateForCard(),
+  },
+  {
+    pic: "./img/AppleImg/MacBook_Pro_4.jpg",
+    model: "Xiaomi",
+    date: getDateForCard(),
+  },
+];
+
+//функция создания карточки
+const generateXiaomiCard = (pic, model, date) => {
+  return `
+  <div class="xiaomi__card section__card">
+      <img class = "xiaomi__card-pic section__card-pic" src=${pic} alt="macbook pro" onClick="imgsrc();">
+      <h3 class="xiaomi__card-title section__card-title">
+        ${model}
+      </h3>
+      <p class="apple_xiaomi-subtitle section__card-subtitle">
+        <div class = "date"> ${date} </div>
+      </p>
+      <div class = 'bt__buy-block'>
+      <button class="bt__buy">Купить</button>
+      </div>
+  </div>
+  `;
+};
+
+const xiaomiCardHTML = xiaomiCardList
+  .map((card) => {
+    return generateXiaomiCard(card.pic, card.model, card.year);
+  })
+  .join("");
+
+xiaomiCards.innerHTML = xiaomiCardHTML;
+//----------------------------------------------------------------------------
+//footer block
+
+const footerItemsArray = [
+  { navPoint: "Apple" },
+  { navPoint: "Samsung" },
+  { navPoint: "Xiaomi" },
+];
+
+//функция создания элементов меню навигации
+const createFooterHTMLList = (navPoint) => {
+  return `<li class = "footer__item">
+  <a href = "#${navPoint}" class = "footer__link">
+  ${navPoint}
+  </a>
+  </li>`;
+};
+
+//создаем переменную с HTML кодом из функции
+const footerListHTML = footerItemsArray
+  .map((navPointName) => {
+    return createFooterHTMLList(navPointName.navPoint);
+  })
+  .join("");
+//вставляем HTML в footerList
+footerList.innerHTML = footerListHTML;
+
+//order__form
+
+const colorArray = [
+    { color: "Black" }, 
+    { color: "White" }, 
+    { color: "Gold" },
+    { color: 'Grey'}
+];
+
+function createOrderColorHTMLList(color) {
+  return `
+    <li class="color__item">
+        <label for="color">${color}</label>
+        <input type="radio" id="color" name="color" value="${color}" />
+    </li>
+    `;
+}
+
+const orderColorListHTML = colorArray
+  .map((color) => {
+    return createOrderColorHTMLList(color.color);
+  })
+  .join("");
+
+colorGroup.innerHTML = orderColorListHTML;
+//----------------------------------------------------------------------------
+// получение даты
+function getDateForCard(date) {
+  const day = new Date(date);
+  const weekday = day.getDay();
+  const months = [
+    "Января",
+    "Февраля",
+    "Марта",
+    "Апреля",
+    "Мая",
+    "Июня",
+    "Июля",
+    "Августа",
+    "Сентября",
+    "Октября",
+    "Ноября",
+    "Декабря",
+  ];
+
+  const year = day.getFullYear();
+  let week = weekday - 1;
+  if (week < 0) {
+    week = 6;
+  }
+
+  return `${months[week]}${year},${week} неделя`;
+}
+//----------------------------------------------------------------------------
 // плавная прокрутка
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 for (let smoothLink of smoothLinks) {
@@ -153,7 +545,7 @@ for (let smoothLink of smoothLinks) {
     });
   });
 }
-
+//----------------------------------------------------------------------------
 //кнопка поднятия вверх
 const btnUp = {
   el: document.querySelector(".btn__up"),
@@ -184,3 +576,6 @@ const btnUp = {
     };
   },
 };
+
+btnUp.addEventListener();
+//----------------------------------------------------------------------------
