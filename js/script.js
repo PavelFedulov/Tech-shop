d115f8f012f9d0e671d819fb9a319a42aad2d3c0
Pavel <125391319+PavelFedulov@users.noreply.github.com>
2023-05-23 12:37:14 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//переменные тэгов
+
 //общие тэги
 const body = document.querySelector("body");
 const wrapper = document.createElement("div");
@@ -7,9 +9,9 @@ const wrapperFullXiaomi = document.createElement("div");
 const wrapperFooter = document.createElement("div");
 const buttonUp = document.createElement("div");
 const buttonUpPic = document.createElement("img");
-//----------------------------------------------------------------------------
-
-//переменные тэгов
+const opacityBlock = document.createElement('div');
+const buttonBuy = document.getElementsByClassName('bt__buy');
+//const buttonBuy = document.createElement('button');
 
 //header
 const header = document.createElement("header");
@@ -74,6 +76,8 @@ const formCloseButton = document.createElement("button");
 buttonUp.classList.add("btn__up", "btn__up-hide");
 buttonUpPic.classList.add("btn__up-pic");
 buttonUpPic.setAttribute("src", "../img/btn-up.png");
+//opacityBlock.classList.add('opacity');
+//buttonBuy.classList.add('bt__buy');
 
 //header
 header.classList.add("header");
@@ -120,7 +124,7 @@ footerTitle.classList.add("footer__title");
 
 //order__form
 
-formBody.classList.add("form__body", "form__body-hide");
+formBody.classList.add("form__body", 'form__body-hide');
 form.classList.add("form");
 orderForm.classList.add("order__form");
 orderWrapper.classList.add("order__wrapper");
@@ -173,10 +177,15 @@ formCloseButton.textContent = "CLOSE";
 //общие
 
 buttonUp.appendChild(buttonUpPic);
+body.appendChild(opacityBlock);
+opacityBlock.appendChild(header);
+opacityBlock.appendChild(appleSection);
+opacityBlock.appendChild(samsungSection);
+opacityBlock.appendChild(xiaomiSection);
+opacityBlock.appendChild(footer);
 
 //header
 body.appendChild(buttonUp);
-body.appendChild(header);
 header.appendChild(wrapper);
 wrapper.appendChild(headerWrapper);
 headerWrapper.appendChild(headerLogo);
@@ -187,28 +196,24 @@ headerNav.appendChild(headerList);
 buttonUp.appendChild(buttonUpPic);
 
 //section: apple
-body.appendChild(appleSection);
 appleSection.appendChild(wrapperFullApple);
 wrapperFullApple.appendChild(appleCaption);
 appleCaption.appendChild(appleTitle);
 wrapperFullApple.appendChild(appleCards);
 
 //section: samsung
-body.appendChild(samsungSection);
 samsungSection.appendChild(wrapperFullSamsung);
 wrapperFullSamsung.appendChild(samsungCaption);
 samsungCaption.appendChild(samsungTitle);
 wrapperFullSamsung.appendChild(samsungCards);
 
 //section: xiaomi
-body.appendChild(xiaomiSection);
 xiaomiSection.appendChild(wrapperFullXiaomi);
 wrapperFullXiaomi.appendChild(xiaomiCaption);
 xiaomiCaption.appendChild(xiaomiTitle);
 wrapperFullXiaomi.appendChild(xiaomiCards);
 
 //footer
-body.appendChild(footer);
 footer.appendChild(wrapperFooter);
 wrapperFooter.appendChild(footerLogo);
 footerLogo.appendChild(footerTitle);
@@ -241,14 +246,14 @@ formButtons.appendChild(formCloseButton);
 
 //массив объектов headerList
 const headerItemsArray = [
-  { navPoint: "Apple" },
-  { navPoint: "Samsung" },
-  { navPoint: "Xiaomi" },
+    { navPoint: "Apple" },
+    { navPoint: "Samsung" },
+    { navPoint: "Xiaomi" },
 ];
 
 //функция создания элементов меню навигации
 const createHeaderHTMLList = (navPoint) => {
-  return `<li class = "header__item">
+    return `<li class = "header__item">
     <a href = "#${navPoint}" class = "header__link">
     ${navPoint}
     </a>
@@ -257,10 +262,10 @@ const createHeaderHTMLList = (navPoint) => {
 
 //создаем переменную с HTML кодом из функции
 const headerListHTML = headerItemsArray
-  .map((navPointName) => {
-    return createHeaderHTMLList(navPointName.navPoint);
-  })
-  .join("");
+    .map((navPointName) => {
+        return createHeaderHTMLList(navPointName.navPoint);
+    })
+    .join("");
 //вставляем HTML в headerList
 headerList.innerHTML = headerListHTML;
 
@@ -270,7 +275,7 @@ headerList.innerHTML = headerListHTML;
 
 //функция создания карточки
 const generateAppleCard = (pic, model, date) => {
-  return `
+    return `
   <div class="apple__card section__card">
       <img class = "apple__card-pic section__card-pic" src=${pic} alt="macbook pro" onClick="imgsrc();">
       <h3 class="apple__card-title section__card-title">
@@ -280,93 +285,91 @@ const generateAppleCard = (pic, model, date) => {
         ${date}
       </p>
       <div class = 'bt__buy-block'>
-      <button class="bt__buy">Купить</button>
+      <button class="bt__buy">BUY</button>
       </div>
   </div>
   `;
 };
 
 //массив объектов карточек apple
-const appleCardList = [
-  {
-    pic: "./img/AppleImg/MacBook_Pro_2.jpg",
-    model: "MacBook Pro",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/iPhone_12_1.jpg",
-    model: "iPhone 12",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/Apple_Watch_1.jpg",
-    model: "Apple Watch Series 8",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/MacBook_Pro_1.jpg",
-    model: "MacBook Pro",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/MacBook_Pro_3.jpg",
-    model: "MacBook Pro",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/MacBook_Pro_4.jpg",
-    model: "MacBook Pro",
-    date: getDateForCard(),
-  },
+const appleCardList = [{
+        pic: "./img/AppleImg/MacBook_Pro_2.jpg",
+        model: "MacBook Pro",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/iPhone_12_1.jpg",
+        model: "iPhone 12",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/Apple_Watch_1.jpg",
+        model: "Apple Watch Series 8",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/MacBook_Pro_1.jpg",
+        model: "MacBook Pro",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/MacBook_Pro_3.jpg",
+        model: "MacBook Pro",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/MacBook_Pro_4.jpg",
+        model: "MacBook Pro",
+        date: getDateForCard(),
+    },
 ];
 
 const appleCardHTML = appleCardList
-  .map((card) => {
-    return generateAppleCard(card.pic, card.model, card.year);
-  })
-  .join("");
+    .map((card) => {
+        return generateAppleCard(card.pic, card.model, card.year);
+    })
+    .join("");
 
 appleCards.innerHTML = appleCardHTML;
 //----------------------------------------------------------------------------
 //samsung block start
 
 //массив объектов карточек samsung
-const samsungCardList = [
-  {
-    pic: "./img/AppleImg/MacBook_Pro_2.jpg",
-    model: "Samsung",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/iPhone_12_1.jpg",
-    model: "Samsung",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/Apple_Watch_1.jpg",
-    model: "Samsung",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/MacBook_Pro_1.jpg",
-    model: "Samsung",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/MacBook_Pro_3.jpg",
-    model: "Samsung",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/MacBook_Pro_4.jpg",
-    model: "Samsung",
-    date: getDateForCard(),
-  },
+const samsungCardList = [{
+        pic: "./img/AppleImg/MacBook_Pro_2.jpg",
+        model: "Samsung",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/iPhone_12_1.jpg",
+        model: "Samsung",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/Apple_Watch_1.jpg",
+        model: "Samsung",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/MacBook_Pro_1.jpg",
+        model: "Samsung",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/MacBook_Pro_3.jpg",
+        model: "Samsung",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/MacBook_Pro_4.jpg",
+        model: "Samsung",
+        date: getDateForCard(),
+    },
 ];
 
 //функция создания карточки
 const generateSamsungCard = (pic, model, date) => {
-  return `
+    return `
   <div class="samsung__card section__card">
       <img class = "samsung__card-pic section__card-pic" src=${pic} alt="macbook pro" onClick="imgsrc();">
       <h3 class="samsung__card-title section__card-title">
@@ -376,59 +379,58 @@ const generateSamsungCard = (pic, model, date) => {
         ${date}
       </p>
       <div class = 'bt__buy-block'>
-      <button class="bt__buy">Купить</button>
+      <button class="bt__buy">BUY</button>
       </div>
   </div>
   `;
 };
 
 const samsungCardHTML = samsungCardList
-  .map((card) => {
-    return generateSamsungCard(card.pic, card.model, card.year);
-  })
-  .join("");
+    .map((card) => {
+        return generateSamsungCard(card.pic, card.model, card.year);
+    })
+    .join("");
 
 samsungCards.innerHTML = samsungCardHTML;
 //----------------------------------------------------------------------------
 //xiaomi block start
 
 //массив объектов карточек xiaomi
-const xiaomiCardList = [
-  {
-    pic: "./img/AppleImg/MacBook_Pro_2.jpg",
-    model: "Xiaomi",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/iPhone_12_1.jpg",
-    model: "Xiaomi",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/Apple_Watch_1.jpg",
-    model: "Xiaomi",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/MacBook_Pro_1.jpg",
-    model: "Xiaomi",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/MacBook_Pro_3.jpg",
-    model: "Xiaomi",
-    date: getDateForCard(),
-  },
-  {
-    pic: "./img/AppleImg/MacBook_Pro_4.jpg",
-    model: "Xiaomi",
-    date: getDateForCard(),
-  },
+const xiaomiCardList = [{
+        pic: "./img/AppleImg/MacBook_Pro_2.jpg",
+        model: "Xiaomi",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/iPhone_12_1.jpg",
+        model: "Xiaomi",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/Apple_Watch_1.jpg",
+        model: "Xiaomi",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/MacBook_Pro_1.jpg",
+        model: "Xiaomi",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/MacBook_Pro_3.jpg",
+        model: "Xiaomi",
+        date: getDateForCard(),
+    },
+    {
+        pic: "./img/AppleImg/MacBook_Pro_4.jpg",
+        model: "Xiaomi",
+        date: getDateForCard(),
+    },
 ];
 
 //функция создания карточки
 const generateXiaomiCard = (pic, model, date) => {
-  return `
+    return `
   <div class="xiaomi__card section__card">
       <img class = "xiaomi__card-pic section__card-pic" src=${pic} alt="macbook pro" onClick="imgsrc();">
       <h3 class="xiaomi__card-title section__card-title">
@@ -438,31 +440,31 @@ const generateXiaomiCard = (pic, model, date) => {
         <div class = "date"> ${date} </div>
       </p>
       <div class = 'bt__buy-block'>
-      <button class="bt__buy">Купить</button>
+      <button class="bt__buy">BUY</button>
       </div>
   </div>
   `;
 };
 
 const xiaomiCardHTML = xiaomiCardList
-  .map((card) => {
-    return generateXiaomiCard(card.pic, card.model, card.year);
-  })
-  .join("");
+    .map((card) => {
+        return generateXiaomiCard(card.pic, card.model, card.year);
+    })
+    .join("");
 
 xiaomiCards.innerHTML = xiaomiCardHTML;
 //----------------------------------------------------------------------------
 //footer block
 
 const footerItemsArray = [
-  { navPoint: "Apple" },
-  { navPoint: "Samsung" },
-  { navPoint: "Xiaomi" },
+    { navPoint: "Apple" },
+    { navPoint: "Samsung" },
+    { navPoint: "Xiaomi" },
 ];
 
 //функция создания элементов меню навигации
 const createFooterHTMLList = (navPoint) => {
-  return `<li class = "footer__item">
+    return `<li class = "footer__item">
   <a href = "#${navPoint}" class = "footer__link">
   ${navPoint}
   </a>
@@ -471,24 +473,24 @@ const createFooterHTMLList = (navPoint) => {
 
 //создаем переменную с HTML кодом из функции
 const footerListHTML = footerItemsArray
-  .map((navPointName) => {
-    return createFooterHTMLList(navPointName.navPoint);
-  })
-  .join("");
+    .map((navPointName) => {
+        return createFooterHTMLList(navPointName.navPoint);
+    })
+    .join("");
 //вставляем HTML в footerList
 footerList.innerHTML = footerListHTML;
 
 //order__form
 
 const colorArray = [
-    { color: "Black" }, 
-    { color: "White" }, 
+    { color: "Black" },
+    { color: "White" },
     { color: "Gold" },
-    { color: 'Grey'}
+    { color: 'Grey' }
 ];
 
 function createOrderColorHTMLList(color) {
-  return `
+    return `
     <li class="color__item">
         <label for="color">${color}</label>
         <input type="radio" id="color" name="color" value="${color}" />
@@ -497,85 +499,115 @@ function createOrderColorHTMLList(color) {
 }
 
 const orderColorListHTML = colorArray
-  .map((color) => {
-    return createOrderColorHTMLList(color.color);
-  })
-  .join("");
+    .map((color) => {
+        return createOrderColorHTMLList(color.color);
+    })
+    .join("");
 
 colorGroup.innerHTML = orderColorListHTML;
 //----------------------------------------------------------------------------
 // получение даты
 function getDateForCard(date) {
-  const day = new Date(date);
-  const weekday = day.getDay();
-  const months = [
-    "Января",
-    "Февраля",
-    "Марта",
-    "Апреля",
-    "Мая",
-    "Июня",
-    "Июля",
-    "Августа",
-    "Сентября",
-    "Октября",
-    "Ноября",
-    "Декабря",
-  ];
+    const day = new Date(date);
+    const weekday = day.getDay();
+    const months = [
+        "Января",
+        "Февраля",
+        "Марта",
+        "Апреля",
+        "Мая",
+        "Июня",
+        "Июля",
+        "Августа",
+        "Сентября",
+        "Октября",
+        "Ноября",
+        "Декабря",
+    ];
 
-  const year = day.getFullYear();
-  let week = weekday - 1;
-  if (week < 0) {
-    week = 6;
-  }
+    const year = day.getFullYear();
+    let week = weekday - 1;
+    if (week < 0) {
+        week = 6;
+    }
 
-  return `${months[week]}${year},${week} неделя`;
+    return `${months[week]}${year},${week} неделя`;
 }
 //----------------------------------------------------------------------------
 // плавная прокрутка
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 for (let smoothLink of smoothLinks) {
-  smoothLink.addEventListener("click", function (e) {
-    e.preventDefault();
-    const id = smoothLink.getAttribute("href");
+    smoothLink.addEventListener("click", function(e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute("href");
 
-    document.querySelector(id).scrollIntoView({
-      behavior: "smooth",
-      block: "start",
+        document.querySelector(id).scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
     });
-  });
 }
 //----------------------------------------------------------------------------
 //кнопка поднятия вверх
 const btnUp = {
-  el: document.querySelector(".btn__up"),
-  show() {
-    // удаляет класс btn__up-hide
-    this.el.classList.remove("btn__up-hide");
-  },
-  hide() {
-    // добавляет класс btn__up-hide
-    this.el.classList.add("btn__up-hide");
-  },
-  addEventListener() {
-    // при прокрутке страницы
-    window.addEventListener("scroll", () => {
-      // определяет величину прокрутки
-      const scrollY = window.scrollY || document.documentElement.scrollTop;
-      // если страница прокручена больше чем на 100px, то отображаем кнопку, иначе скрываем
-      scrollY > 100 ? this.show() : this.hide();
-    });
-    // при нажатии на кнопку btn__up
-    document.querySelector(".btn__up").onclick = () => {
-      // поднимаемся в начало страницы
-      window.scrollTo({
-        top: 0,
-        lefr: 0,
-        behavior: "smooth",
-      });
-    };
-  },
+    el: document.querySelector(".btn__up"),
+    show() {
+        // удаляет класс btn__up-hide
+        this.el.classList.remove("btn__up-hide");
+    },
+    hide() {
+        // добавляет класс btn__up-hide
+        this.el.classList.add("btn__up-hide");
+    },
+    addEventListener() {
+        // при прокрутке страницы
+        window.addEventListener("scroll", () => {
+            // определяет величину прокрутки
+            const scrollY = window.scrollY || document.documentElement.scrollTop;
+            // если страница прокручена больше чем на 100px, то отображаем кнопку, иначе скрываем
+            scrollY > 100 ? this.show() : this.hide();
+        });
+        // при нажатии на кнопку btn__up
+        document.querySelector(".btn__up").onclick = () => {
+            // поднимаемся в начало страницы
+            window.scrollTo({
+                top: 0,
+                lefr: 0,
+                behavior: "smooth",
+            });
+        };
+    },
 };
 
 btnUp.addEventListener();
 //----------------------------------------------------------------------------
+// форма покупки
+
+function successfullyBuy(e) {
+    e.preventDefault();
+    alert('Purchase successfully completed! Thank you!');
+};
+
+function hideOrderForm(e) {
+    e.preventDefault();
+    formBody.classList.add('form__body-hide')
+    opacityBlock.classList.remove('opacity')
+
+};
+
+formBuyButton.addEventListener('click', successfullyBuy);
+
+function showOrderForm(e) {
+    e.preventDefault();
+    formBody.classList.remove('form__body-hide')
+    opacityBlock.classList.add('opacity')
+
+};
+//formBody.classList.add('.form__body-hide');
+//opacityBlock.classList.remove('.opacity');
+for (let i = 0; i < buttonBuy.length; i++) {
+    buttonBuy[i].addEventListener('click', showOrderForm);
+}
+
+formCloseButton.addEventListener('click', hideOrderForm);
+//formCloseButton.addEventListener('click', hideOrderForm);
